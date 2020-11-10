78148b2e289e387328ab984ee53f7d99071c8f88
Zachary Jehle <zacharyjehle@zacharys-mbp.home>
2020-11-09 19:59:14 -0500

    initial commit

---
+++ covidcases/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=1024)
+    cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
+
+class State(models.Model):
+    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=1024)
+    cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
+
+class County(models.Model):
+    state_id = models.ForeignKey(State, on_delete=models.CASCADE)
+    name = models.CharField(max_length=1024)
+    cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
